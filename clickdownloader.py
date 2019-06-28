498d7339e260d8d7545a34706434942fb120017f
jordiprats <jordi.prats@gmail.com>
2019-06-28 18:56:12 +0200

    skel download

--- clickdownloader.py
+++ clickdownloader.py
@@ -17,19 +17,31 @@ debug = False
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    # f.flush()
+    return local_filename
+
 def getAlbum(session, base_url, album_id):
     global debug
     # /students/albums_fotos.php?accio=veure&id=1441
     album_response = session.get(base_url+'/students/albums_fotos.php?accio=veure&id='+album_id)
     if debug:
-        print("album response code: " + str(album_response.status_code))
-        # print("album response text: " + str(album_response.text))
+        eprint("album response code: " + str(album_response.status_code))
+        # eprint("album response text: " + str(album_response.text))
 
     # <div><h2 class="Gran head_news_interior"><strong>...</strong></h2></div>
     pattern_get_nom_album = re.compile(r'<div><h2 class="Gran head_news_interior"><strong>([^<]*)<')
     titol_album = re.findall(pattern_get_nom_album, str(album_response.text))[0]
     # if debug:
-    #     print("nom album: "+titol_album)
+    #     eprint("nom album: "+titol_album)
 
     # <a class="boto_vermell_petit" href="...zip">Descarregar àlbum</a>
     pattern_get_url_download = re.compile(r'<a class="boto_vermell_petit" href="([^"]*)">')
@@ -39,10 +51,10 @@ def getAlbum(session, base_url, album_id):
         album_url_download = ''
 
     # if debug:
-    #     print("URL DOWNLOAD: "+album_url_download)
+    #     eprint("URL DOWNLOAD: "+album_url_download)
 
     if debug:
-        print("nom album: "+titol_album +" URL DOWNLOAD: "+album_url_download)
+        eprint("nom album: "+titol_album +" URL DOWNLOAD: "+album_url_download)
 
 if __name__ == '__main__':
 
@@ -98,8 +110,8 @@ if __name__ == '__main__':
     login_url_response = session.post(base_url+login_url, data=data_login)
 
     if debug:
-        print("login_url response code: " + str(login_url_response.status_code))
-        # print("login_url response text: " + str(login_url_response.text))
+        eprint("login_url response code: " + str(login_url_response.status_code))
+        # eprint("login_url response text: " + str(login_url_response.text))
 
     numero_pagina=1
     llistat_albums=[]
@@ -114,9 +126,9 @@ if __name__ == '__main__':
         index_url_response = session.get(base_url+index_url+'?accio=llistar&pag='+str(numero_pagina)+'&lloc=fotos')
 
         if debug:
-            print("URL: "+base_url+index_url+'?accio=llistar&pag='+str(numero_pagina)+'&lloc=fotos')
-            print("index_url response code: " + str(index_url_response.status_code))
-            # print("index_url response text: " + str(index_url_response.text))
+            eprint("URL: "+base_url+index_url+'?accio=llistar&pag='+str(numero_pagina)+'&lloc=fotos')
+            eprint("index_url response code: " + str(index_url_response.status_code))
+            # eprint("index_url response text: " + str(index_url_response.text))
 
         pattern_url_albums = re.compile(r'<a href="([^>]*albums_fotos.php[^>]*veure[^>]*)">')
 
